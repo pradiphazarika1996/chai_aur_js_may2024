49636a9326d26a9b0b32e43a03d62afdc79b4d0b
pradiphazarika1996 <151366559+pradiphazarika1996@users.noreply.github.com>
2024-05-08 11:04:57 +0000

    scope commit

--- 01_basic/one.js
+++ 01_basic/one.js
@@ -1,2 +1,13 @@
-const fName = "pradip";
-console.log(fName);
+const accountId = 88;
+// accountId = 99;
+
+let accountEmail = "pradip@google.com";
+accountEmail = "rahul@gmail.com"
+accountEmail="akash@gmail.com"
+
+var accountName = "pradip hazarika";
+accountName="rahul";
+accountCity = "guwahati";
+let accountPassword ;
+
+console.table([accountId,accountEmail,accountName,accountCity,accountPassword])
