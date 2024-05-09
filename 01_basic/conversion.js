5ed95c6ff347110dfe469006ec4f682063e85c3d
pradiphazarika1996 <151366559+pradiphazarika1996@users.noreply.github.com>
2024-05-09 11:53:42 +0000

    conversion push

--- 01_basic/conversion.js
+++ 01_basic/conversion.js
@@ -56,13 +56,18 @@ let str3 = str1 + str2
 // console.log(+true);
 // console.log(+"");
 
-let num1, num2, num3
+// let num1, num2, num3
 
-num1 = num2 = num3 = 2 + 2
+// num1 = num2 = num3 = 2 + 2
 
-let gameCounter = 100
-++gameCounter;
-console.log(gameCounter);
+// let gameCounter = 100
+// gameCounter++;
+// console.log(gameCounter);
+
+// console.log(null >0);
+// console.log(null >=0);
+// console.log(null >0);
+// console.log("2" === 2); => strictly check values and types
 
 // link to study
 // https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
