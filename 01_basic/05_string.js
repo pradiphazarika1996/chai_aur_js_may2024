126c19b129b10f35d9fc5c9e7be7f1d65e77f02e
pradiphazarika1996 <151366559+pradiphazarika1996@users.noreply.github.com>
2024-05-10 08:45:11 +0000

    string commit

---
+++ 01_basic/05_string.js
@@ -0,0 +1,19 @@
+const studentName = "     ajay      ";
+const age = 34;
+
+// console.log(`Hello my name is ${studentName.toUpperCase()} and my age is ${age}`);
+
+// console.log(studentName.charAt(3));
+// console.log(studentName.indexOf('y'));
+// console.log(studentName.substring(0,2));
+// console.log(studentName.slice(0,2));
+// console.log(studentName);
+// console.log(studentName.trim());
+
+const url = "https://pradip%2090.com";
+const newUrl = url.replace('%20', '-')
+// console.log(newUrl);
+const result = url.includes("hazarika");
+// console.log(result);
+// split 
+
