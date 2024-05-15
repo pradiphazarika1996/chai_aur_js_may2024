cce7fa42e7cbd204d1140eee65f2c07ef87b7079
pradiphazarika1996 <151366559+pradiphazarika1996@users.noreply.github.com>
2024-05-15 11:01:55 +0000

    obj2 comit

---
+++ 03_basic/obj1.js
@@ -0,0 +1,17 @@
+const tinderUser = {}
+tinderUser.id = "123"
+tinderUser.name="akash"
+tinderUser.age = 25
+// console.log(tinderUser);
+
+const obj1 = {1:"a",2:"b"}
+const obj2 = {3:"c", 4:"d"}
+const obj4 = {5:"e", 6:"f"}
+
+// const obj3 = {...obj1, ...obj2}
+// const obj3 = Object.assign({}, obj1, obj2,obj4)
+// console.log(obj3);
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(tinderUser.hasOwnProperty('age'));
