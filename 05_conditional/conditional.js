3c7c9e5c93e39155a1033962625eaf93204e6703
pradiphazarika1996 <151366559+pradiphazarika1996@users.noreply.github.com>
2024-05-16 17:36:39 +0000

    conditional commit

---
+++ 05_conditional/conditional.js
@@ -0,0 +1,55 @@
+const score = 100;
+
+// if (score <60) {
+//     console.log("executed");
+    
+// } else {
+//     console.log("not executed");
+    
+// }
+
+// ternary operator
+
+// const result = (score <50)?"executed":"not executed"
+// console.log(result);
+
+const month = 2;
+
+// switch (month) {
+//     case 1:
+//         console.log("january");
+        
+//         break;
+//         case 2:
+//         console.log("feb");
+        
+//         break;
+
+//         case 3:
+//         console.log("march");
+        
+//         break;
+//         case 4:
+//             console.log("april");
+            
+//             break;
+
+
+//     default:
+//         console.log("sunday");
+//         break;
+// }
+
+const myObj = {};
+
+if(Object.keys(myObj).length === 0){
+    // console.log("empty object");
+    
+}
+
+// nullish coalesence operator
+
+let val1 ;
+val1 = 5 ?? 10;
+val1 = null ?? 10;
+// console.log(val1);
