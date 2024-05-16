f89537e4c402d9eb9b9c7e4dd6545bf59d3dc6a0
pradiphazarika1996 <151366559+pradiphazarika1996@users.noreply.github.com>
2024-05-16 07:33:01 +0000

    arrow functin commit

--- 04_basic/scopes.js
+++ 04_basic/scopes.js
@@ -6,10 +6,44 @@ if (true) {
     const a = 10;
     let b = 20;
     var c = 30;
-    console.log(a);
+    // console.log(a);
     
 }
 
 // console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+function one(){
+    let username = "pradip";
+    function two(){
+        let website ="youtube";
+        // console.log(username);
+    }
+    // console.log(website);
+    two()
+}
+one()
+
+if (true) {
+    const username = "akash";
+    if(username === "akash"){
+        const website = "youtubevideo";
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+function numOne(num1){
+    return num1 + 1;
+}
+console.log(numOne(5));
+
+
+const numTwo =(num1)=> {
+    return num1 + 2
+}
+const res = numTwo(5)
+console.log(res);
