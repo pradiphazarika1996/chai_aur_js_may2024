43893c28ded107961f6bdac5f38baf19ac7df180
pradiphazarika1996 <151366559+pradiphazarika1996@users.noreply.github.com>
2024-05-17 06:15:51 +0000

    ite

---
+++ 05_conditional/iteration.js
@@ -0,0 +1,19 @@
+// const number = [1,2,3,4,5,6,7,8,9,10];
+
+// const result = number.filter((val)=>{
+//     // if(val >5){
+//     //     console.log(val);
+//     // }
+//     return val >5
+// })
+// console.log(result);
+
+
+// reduce method in js 
+
+// const array1 = [1,2,3,4];
+
+// const result = array1.reduce((acc,currVal)=>{
+//     return acc+currVal
+// })
+// console.log(result);
