294a1fb64c8f2ca53f3fdd6c46507c05d7d309e0
pradiphazarika1996 <151366559+pradiphazarika1996@users.noreply.github.com>
2024-05-17 04:12:25 +0000

    loops

---
+++ 05_conditional/loop.js
@@ -0,0 +1,41 @@
+const marks = [100,200,300,600];
+
+// for (let i = 0; i < marks.length; i++) {
+//    console.log(marks[i]);
+    
+// }
+
+
+// for (let i = 0; i < 10; i++) {
+//     const element = i;
+//     if (element === 5 ) {
+//         console.log("5 is best number");
+        
+//     }
+//     console.log(element);
+    
+// }
+
+// loop table
+
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`outer loop ${i}`);
+//     for (let j = 1; j <= 10; j++) {
+       
+//    console.log(i + '*' + j + '=' + i * j);
+        
+//     }
+  
+    
+// }
+
+for (let i = 1; i <= 20; i++) {
+    if (i === 5) {
+        console.log(i);
+
+        break;
+    }
+   console.log(`value of i is : ${i}`);
+    
+}
