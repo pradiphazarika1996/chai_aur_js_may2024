b167a9b0299f8dcfcd30c64c552a192e23542cd0
pradiphazarika1996 <151366559+pradiphazarika1996@users.noreply.github.com>
2024-05-17 06:32:24 +0000

    reduce method commit

--- 05_conditional/iteration.js
+++ 05_conditional/iteration.js
@@ -13,7 +13,36 @@
 
 // const array1 = [1,2,3,4];
 
+// const initialValue = 0;
+
 // const result = array1.reduce((acc,currVal)=>{
 //     return acc+currVal
-// })
+// },initialValue)
 // console.log(result);
+
+
+// const number = [1,2,3,4,5,6,7,8,9];
+// const result = number.reduce((acc, currValue)=>acc + currValue, 0);
+// console.log(result);
+
+
+// const shoppingCart = [
+//     {
+//         course :"js course",
+//         price:3000
+//     },
+//     {
+//         course :"js course",
+//         price:4500
+//     },
+//     {
+//         course :"js course",
+//         price:8000
+//     },
+//     {
+//         course :"js course",
+//         price:7000
+//     },
+// ];
+// const result = shoppingCart.reduce((acc,item)=> acc + item.price,0)
+console.log(result);
