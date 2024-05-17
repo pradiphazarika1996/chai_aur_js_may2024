8c437a4cc61d18c51cd3b9d5a6425b58160fa8b2
pradiphazarika1996 <151366559+pradiphazarika1996@users.noreply.github.com>
2024-05-17 05:08:48 +0000

    loop

--- 05_conditional/loop.js
+++ 05_conditional/loop.js
@@ -63,3 +63,66 @@ const marks = [100,200,300,600];
 //     console.log(`score is : ${score}`);
 //     score++;
 // } while (score <= 10);
+
+// const arr1 = ['a', 'b', 'c'];
+
+// for (const element of arr1) {
+//     console.log(element);
+    
+// }
+// for (const key in arr1) {
+//     console.log(key);
+// }
+
+// let arr = [1,2,3];
+
+// for (let value of arr) {
+//     value += 1;
+//     console.log(value);
+    
+// }
+
+
+// convert object into array and iterate all the elements just for practise
+
+// let student = {
+//     name:"pradip",
+//     age:27,
+//     dept:"software engineer"
+// }
+
+// const result = Object.entries(student).map((val)=>{
+//     return val;
+
+// })
+
+// console.log(result[0]);
+
+
+// let numbers = [1, 2, 3, 3, 4, 5, 5];
+
+// let uniqueNumbers = [...new Set(numbers)];
+
+// console.log(uniqueNumbers);
+
+
+
+// remove duplicate value using set method
+
+
+
+// let number = [1,2,3,2,5,7,4,2,3];
+
+// let result = [...new Set(number)]
+// result.sort()
+// console.log(result);
+
+
+// const cricket = ['sehwag', 'virat','dhoni', 'patel'];
+
+// const res = cricket.map((val)=>{
+//     return val
+    // console.log(val);
+// })
+
+// console.log(res);
